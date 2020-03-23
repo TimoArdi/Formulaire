bdebbe873bdb609a4cf9db5d35c49f87c42c825b
TimoArdi <61334551+TimoArdi@users.noreply.github.com>
2020-03-23 17:12:34 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<?php
+echo  $_POST['user_name'];
+?>
+
+<form  action="thanks.php"  method="post">
+    <div>
+        <label  for="nom">Nom :</label>
+        <input  type="text"  id="name"  name="name">
+    </div>
+    <div>
+        <label  for="Prenom">Prenom :</label>
+        <input  type="text"  id="firstname"  name="user_name">
+    </div>
+    <div>
+        <label  for="courriel">Courriel :</label>
+        <input  type="email"  id="courriel"  name="user_email">
+    </div>
+    <label for="phone">Numero de telephone :</label>
+
+    <input type="tel" id="phone" name="user_phone">
+    <div>
+        <label  for="message">Message :</label>
+        <textarea  id="message"  name="user_message"></textarea>
+    </div>
+    <div>
+        <select name="user_reasons" id="reasons-select">
+            <option value="1">Demande de rendez-vous</option>
+            <option value="2">Assistance</option>
+            <option value="3">Question Produit</option>
+        </select>
+    </div>
+    <div  class="button">
+        <button  type="submit">Envoyer votre message</button>
+    </div>
+</form>
+</body>
+</html>
